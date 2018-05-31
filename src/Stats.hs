9ca35a4e1796f398bc2b4c5bb7a8051bd414bcb6
Axect <edeftg@gmail.com>
2018-06-01 02:15:06 +0900

    Least Square Method

--- src/Stats.hs
+++ src/Stats.hs
@@ -6,9 +6,10 @@ mean :: Fractional a => Vector a -> a
 mean v = sum v / fromIntegral (length v)
 
 cov' :: Floating a => Vector a -> Vector a -> a
-cov' x y | length x <= 1 || length y <= 1 = error "Samples are not enough"
-         | length x /= length y = error "Length is not same"
-         | otherwise = ((x .- mean x) .*. (y .- mean y)) / fromIntegral (length x - 1)
+cov' x y
+  | length x <= 1 || length y <= 1 = error "Samples are not enough"
+  | length x /= length y = error "Length is not same"
+  | otherwise = ((x .- mean x) .*. (y .- mean y)) / fromIntegral (length x - 1)
 
 var :: Floating a => Vector a -> a
 var v = cov' v v
@@ -18,3 +19,12 @@ std = sqrt . var
 
 cov :: Floating a => Vector a -> Vector a -> Matrix a
 cov x y = Vector [[var x, cov' x y], [cov' y x, var y]]
+
+-- Least Square Method
+lm :: Floating a => Vector a -> Vector a -> (a, a)
+lm x y = ((my - b1 * mx), b1)
+ where
+  mx = mean x
+  my = mean y
+  b1 = (x .- mx) .*. (y .- my) / ((x .- mx) .*. (x .- mx))
+
