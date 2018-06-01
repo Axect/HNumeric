ac0a3c66d15fc4e3184bcd47360cb00ac9f70c08
Axect <edeftg@gmail.com>
2018-06-01 15:47:42 +0900

    fromCSV to DataFrame

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,11 @@ module Main where
 
 import           Vector
 import           Stats
+import           CSV
 
 main :: IO ()
 main = do
-    let a = Vector [1, 3, 4]
-    print a
-    print $ map ($ a) [mean, var, std]
+  let a = Vector [1, 3, 4]
+  print a
+  print $ map ($ a) [mean, var, std]
 
---
+++ src/CSV.hs
@@ -0,0 +1,25 @@
+module CSV where
+
+import           Vector
+
+data DataFrame = DataFrame { header :: Vector String
+                           , numeric :: Matrix Double
+                           } deriving (Eq, Show)
+
+-- String to [String] With separator 
+splitWith :: Char -> String -> [String]
+splitWith _ "" = []
+splitWith c s | null ms   = [ws]
+              | otherwise = ws : splitWith c (tail ms)
+ where
+  ws = takeWhile (/= c) s
+  ms = dropWhile (/= c) s
+
+fromCSV :: String -> DataFrame
+fromCSV r = DataFrame {header = Vector h1, numeric = Vector v1}
+ where
+  q1 = filter (/= '"') r
+  q2 = splitWith '\n' q1
+  q3 = map (splitWith ',') q2
+  h1 = head q3
+  v1 = map (map read) (tail q3) :: [[Double]]
