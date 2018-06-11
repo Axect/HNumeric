e04d5db9e2ab1ff8f841acf014f2f99f42aa3945
Axect <edeftg@gmail.com>
2018-06-11 18:21:30 +0900

    Success write dataframe

--- src/HNum/CSV.hs
+++ src/HNum/CSV.hs
@@ -3,9 +3,30 @@ module HNum.CSV where
 import           HNum.Vector
 --import qualified Data.Map.Strict               as M
 
-type Header = String
+type Header = [String]
+type Label = [String]
 
-data DataFrame a = DataFrame { header :: Vector String, dat :: Matrix a, lab :: Vector String } deriving (Show, Eq)
+data DataFrame a = DataFrame { header :: Header, dat :: Matrix a, lab :: Label} deriving (Show, Eq)
+
+-- | No label dataframe
+dataframe :: Header -> Matrix a -> DataFrame a
+dataframe h m | length h == row m = DataFrame h m (replicate n "value")
+              | otherwise         = error "Length of Header != Length of Data"
+  where n = length m `div` length h
+
+-- | With label dataframe
+dataframe' :: Header -> Matrix a -> Label -> DataFrame a
+dataframe' h m l
+  | length h == row m && row m == length l = DataFrame h m l
+  | otherwise = error "Length of Header != Length of Data != Length of Label"
+
+-- | No label dataframe from Vectors
+fromVectors :: Header -> [Vector a] -> DataFrame a
+fromVectors h vs = dataframe h (matrix vs') where vs' = map toList vs
+
+-- | With label dataframe from Vectors
+fromVectors' :: Header -> [Vector a] -> Label -> DataFrame a
+fromVectors' h vs = dataframe' h (matrix vs') where vs' = map toList vs
 
 instance Functor DataFrame where
   fmap f df = df { dat = fmap f (dat df) }
@@ -21,9 +42,18 @@ instance CSVtize Vector where
 instance CSVtize Matrix where
   toString m = foldr ((\x y -> x ++ "\n" ++ y) . cm) "" m1
     where m1 = matForm (show <$> m)
-          cm :: [String] -> String
-          cm [] = []
-          cm (x:xs) | null xs = x
-                    | otherwise = x ++ "," ++ cm xs
   write title m = writeFile title (toString m)
 
+instance CSVtize DataFrame where
+  toString (DataFrame h m l) = h' ++ "\n" ++ m'
+    where h' = cm h ++ ",label"
+          m' = foldr ((\x y -> x ++ "\n" ++ y) . cm) "" $ matForm $ hcat (show <$> transpose m) l'
+          l' = transpose $ matrix [l]
+  write title df = writeFile title (toString df)
+
+
+-- | Convenient 
+cm :: [String] -> String
+cm [] = []
+cm (x : xs) | null xs   = x
+            | otherwise = x ++ "," ++ cm xs
