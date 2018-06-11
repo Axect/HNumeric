a75be1c86f50971c95689d3d6e0a7cd68a7ed49b
Axect <edeftg@gmail.com>
2018-06-11 17:33:09 +0900

    Matrix CSVtize

--- src/HNum/CSV.hs
+++ src/HNum/CSV.hs
@@ -1,11 +1,14 @@
 module HNum.CSV where
 
 import           HNum.Vector
-import qualified Data.Map.Strict               as M
+--import qualified Data.Map.Strict               as M
 
 type Header = String
 
-data DataFrame a = Map Header (Vector a) deriving (Show, Eq)
+data DataFrame a = DataFrame { header :: Vector String, dat :: Matrix a, lab :: Vector String } deriving (Show, Eq)
+
+instance Functor DataFrame where
+  fmap f df = df { dat = fmap f (dat df) }
 
 class Functor f => CSVtize f where
   toString :: Show a => f a -> String
@@ -14,3 +17,13 @@ class Functor f => CSVtize f where
 instance CSVtize Vector where
   toString v = foldr (\x y -> x ++ "\n" ++ y) "" (show <$> v)
   write title v = writeFile title (toString v)
+
+instance CSVtize Matrix where
+  toString m = foldr ((\x y -> x ++ "\n" ++ y) . cm) "" m1
+    where m1 = matForm (show <$> m)
+          cm :: [String] -> String
+          cm [] = []
+          cm (x:xs) | null xs = x
+                    | otherwise = x ++ "," ++ cm xs
+  write title m = writeFile title (toString m)
+
