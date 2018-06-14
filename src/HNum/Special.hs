d9c48e367669f0a4dc86d8462000ef0176504156
Axect <edeftg@gmail.com>
2018-06-14 15:14:49 +0900

    Fix Dist to Special

--- src/HNum/Special.hs
+++ src/HNum/Special.hs
@@ -1,13 +1,16 @@
-module HNum.Dist where
+module HNum.Special where
 
 import           System.Random
 import qualified Numeric.SpecFunctions         as SF
 import           HNum.Vector
 import           HNum.F
 
-class FuncTools f => Special f where
+class FuncTools f => SpecialFunc f where
   erf :: Real a => f a -> f Double
+  invErf :: Real a => f a -> f Double
 
-instance Special Vector where
+instance SpecialFunc Vector where
   erf v = SF.erf . realToFrac <$> v
+  invErf v = SF.invErf . realToFrac <$> v
+
 
